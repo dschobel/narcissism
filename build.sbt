094c38d347f783be030720ffd97d729fefcef2ce
Daniel Schobel <dschobel@gmail.com>
2013-02-12 14:52:42 -0500

    async method to get twitter followers

--- build.sbt
+++ build.sbt
@@ -6,13 +6,17 @@ name := "Narcissism"
 
 version := "ALPHA"
 
-scalaVersion := "2.9.2"
+scalaVersion := "2.10.0"
 
 resolvers += "twitter-repo" at "http://maven.twttr.com"
+
+scalacOptions ++= Seq("-unchecked", "-deprecation")
 
 libraryDependencies ++= Seq(
                 "com.twitter" % "finagle-core" % "1.9.0", 
                 "com.twitter" % "finagle-http" % "1.9.0", 
                 "oauth.signpost" % "signpost-core" % "1.2",
                 "oauth.signpost" % "signpost-commonshttp4" % "1.2", 
-                "org.apache.httpcomponents" % "httpclient" % "4.2")
+                "org.apache.httpcomponents" % "httpclient" % "4.2",
+                "org.scalatest" % "scalatest_2.10.0" % "2.0.M5" % "test"
+                )
---
+++ src/main/scala/TwitterAPI.scala
@@ -0,0 +1,72 @@
+import scala.concurrent.duration._
+import concurrent.{Await, ExecutionContext, Future}
+import ExecutionContext.Implicits.global
+import org.apache.http.util.EntityUtils
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.client.methods.HttpGet
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer
+import scala.util.parsing.json._
+import util.{Success, Failure}
+
+
+object TwitterAPI {
+
+  val AccessToken = "";
+  val AccessSecret = "";
+  val ConsumerKey = "";
+  val ConsumerSecret = "";
+  val consumer = new CommonsHttpOAuthConsumer(ConsumerKey,ConsumerSecret)
+  consumer.setTokenWithSecret(AccessToken, AccessSecret)
+
+  JSON.globalNumberParser = {input : String => BigDecimal(input)}
+
+
+  def getResponseBody(requestString: String): String= {
+    val request = new HttpGet(requestString)
+    consumer.sign(request)
+    val client = new DefaultHttpClient()
+    val response = client.execute(request)
+    val entity = response.getEntity()
+    var respMessage = new String()
+    if(entity != null){
+      respMessage = EntityUtils.toString(entity)
+    }
+    respMessage
+  }
+  def getFutureResponseBody(request: String): Future[String]= Future{getResponseBody(request) }
+
+  /**
+   * Given a twitter screen name, returns the followers, capped at 5000
+   *
+   * @param screen_name
+   * @return
+   */
+  def getFollowers(screen_name: String):Future[List[User]] = {
+    val processJSON = parseIds _ andThen getUsersFromIds
+    val request = s"http://api.twitter.com/1.1/followers/ids.json?cursor=-1&screen_name=$screen_name"
+    getFutureResponseBody(request).map {resp => processJSON(resp)}
+  }
+
+  def parseIds(json: String): List[BigDecimal]={
+    val res = (for (map <- JSON.parseFull(json).asInstanceOf[Option[Map[String,Any]]];
+         list <- map.get("ids").asInstanceOf[Option[List[BigDecimal]]]
+    ) yield list).getOrElse(Nil)
+    res
+  }
+
+  def getUsersFromIds(ids: List[BigDecimal]): List[User] = {
+    ids.map{id => User("desc",id.toString,"loc","name","profile_url","screenname")}
+  }
+
+  def main(args: Array[String]) {
+    val futureFollowers = TwitterAPI.getFollowers("dschobel")
+
+    futureFollowers.onComplete{
+      case Success(followers) =>  println((followers.map {_.id_str}).mkString(", "))
+      case Failure(err) => println("something went wrong\n" + err.toString)
+    }
+
+    println("waiting for result")
+    Await.result(futureFollowers,2 minutes)
+  }
+}
